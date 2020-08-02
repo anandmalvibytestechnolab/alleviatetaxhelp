162fe7d8a3c9b46e888aaca446adf8731fdcbc65
Helvert Mauricio Mateus <helvert.mateus@hotmail.com>
2020-08-01 21:01:23 -0500

    CCPA leads being saved to database

--- app/Http/Controllers/Frontend/CcpaLeadController.php
+++ app/Http/Controllers/Frontend/CcpaLeadController.php
@@ -10,12 +10,40 @@ class CcpaLeadController extends Controller
 {
     public function store()
     {
-        d(request()->all());
         $rules = [
-            'email_address' => ['required', 'email']
+            'email_address' => ['required', 'email'],
+            'requestor' => ['nullable'],
+            'requestoptions' => ['nullable'],
+            'first_name' => ['nullable'],
+            'last_name' => ['nullable'],
+            'street' => ['nullable'],
+            'apt' => ['nullable'],
+            'city' => ['nullable'],
+            'state' => ['nullable'],
+            'zip_code' => ['nullable'],
+            'primary_phone' => ['nullable'],
         ];
         $validData = request()->validate($rules);
-        CcpaLead::create($validData);
+        CcpaLead::create($this->buildStoreData($validData));
         return redirect()->route('CCPA_CONFIRMATION_PAGE_SHOW');
+    }
+
+    private function buildStoreData($validData)
+    {
+        return collect($validData)
+            ->merge([
+           'requestoptions' => $this->transformArrayToString($validData['requestoptions'])
+        ])
+            ->toArray();
+    }
+
+    private function transformArrayToString($data)
+    {
+        $data = collect($data);
+        $valueCount = $data->count();
+        if($valueCount === 1){
+            return (string) $data[0];
+        }
+        return $data->implode(',');
     }
 }
