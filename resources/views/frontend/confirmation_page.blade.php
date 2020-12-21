f29d82aef31698e1d6cc4f966ed5d32cc07c1ebd
anandmalvibytestechnolab <anand.malvi@bytestechnolab.in>
2020-12-21 18:15:38 +0530

    GT-9: ATS thank you page content changes & design | status:completed

--- resources/views/frontend/confirmation_page.blade.php
+++ resources/views/frontend/confirmation_page.blade.php
@@ -7,7 +7,12 @@
                 <div class="row justify-content-center">
                     <div class="col-sm-12 text-center p-0 mt-3 mb-2">
                         <div class="card px-0 pt-4 pb-0 mt-3 mb-3">
-                            <form id="msform" action="#" method="post">
+                            <form id="msform" action="#" method="post" class="thank_form">
+                                <a href="/">
+                                    <span>
+                                        <img src="/img/logo.png">
+                                    </span>
+                                </a>
                                 <div class="progre_s" style="text-align:left">
                                     <span style="margin-left:calc(100% - 40px);">100%</span>
                                     <div class="progress" id="progress_tab">
@@ -18,22 +23,43 @@
                                 <br>
                                 <fieldset id="first_slide" class="field_set">
                                     <div class="form-card success">
-                                        <h2 class="text-center px-0">
-                                            <strong>Thank you for submitting your information to us.</strong>
-                                        </h2>
-                                        <br>
                                         <div class="row justify-content-center">
                                             <i class="fa fa-check"></i>
                                         </div>
-                                        <br>
+                                        <h2 class="text-center px-0">
+                                            <strong>Congrats on taking the first step to resolving your tax debt!</strong>
+                                        </h2>
+                                        <h3 class="text-center px-0">
+                                            One of our tax specialists will be reaching out soon via the phone number you submitted.
+                                        </h3>
                                         <div class="row justify-content-center">
+                                            <!--START: Fire ckmpixel-->
                                             @if (request()->query('tax_debt') > 9999)
                                                 <iframe src="{{ config('app.cake_url_iframe') }}?o=2&t=TRANSACTION_ID&r={{ request()->query('reqid') }}" height="1" width="1" frameborder="0"></iframe>
                                             @endif
+                                            <!--END: Fire ckmpixel-->
+                                            <!--
                                             <div class="col-12 text-center">
                                                 <h5 class="purple-text text-center">Someone from Alleviate Tax will be calling you shortly, or when business hours resume. If you would rather call or text now, call
                                                     <a href="tel:+18002353333" target="_blank" class="text-info">18002353333</a> (we’re open Monday – Friday from 8AM – 4:30PM PST) or text
                                                     <strong>“taxhelp”</strong> to <strong>12345</strong></h5>
+                                            </div>
+                                            -->
+                                            <div class="col-12 text-center">
+                                                <p>If you would like to speak to someone immediately, you can call us at <a href="tel:+1888-888-8888" target="_blank" class="text-info">888-888-8888</a>.</p>
+                                                <p>
+                                                    Our offices are open Monday – Friday from 6am – 6pm PST
+                                                </p>
+                                                <p> Or you can schedule a call with us at a time of your choosing: 
+                                                    <a href="https://calendly.com/taxcall" target="_blank" class="text-info">Book Calendar</a>
+                                                </p>
+                                                <iframe class="video_btn"
+                                                    src="https://player.vimeo.com/video/492617133?autoplay=1"
+                                                    webkitallowfullscreen mozallowfullscreen allowfullscreen allow="autoplay">
+                                                </iframe>
+                                                <p class="reminder">
+                                                    As a reminder, if you hire Alleviate for your tax resolution, we’ll take care of your {{ date('Y') }} tax preparation and filing for free!
+                                                </p>
                                             </div>
                                         </div>
                                     </div>
